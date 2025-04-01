c0611837ce227806c2e85481692c5022c4414f89
DENIS <Grachovv12345@gmail.com>
2025-04-01 11:25:36 +0300

    added fixed class diagram added InvertBooleanConverter fixed reports

---
+++ VacTrack/Converter/InvertBooleanConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace VacTrack.Converter
+{
+    class InvertBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b) return !b;
+            else return value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b) return !b;
+            else return value;
+        }
+    }
+}
