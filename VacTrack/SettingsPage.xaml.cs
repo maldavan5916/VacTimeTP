e7c9750e55c7ec270115f2e7e3fdb8fd639993a1
DENIS <Grachovv12345@gmail.com>
2025-04-29 20:30:01 +0300

    user control implemented

--- VacTrack/SettingsPage.xaml.cs
+++ VacTrack/SettingsPage.xaml.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using DatabaseManager;
 using MaterialDesignThemes.Wpf;
 using Microsoft.EntityFrameworkCore;
-using VacTrack.ViewTables;
 
 namespace VacTrack
 {
@@ -17,7 +17,6 @@ namespace VacTrack
     {
         private SettingViewModel ThisViewModel => (SettingViewModel)DataContext;
         public SettingsPage() => InitializeComponent();
-
         public void OnNavigatedFromCache() => ThisViewModel.ReloadData();
     }
 
@@ -37,21 +36,36 @@ namespace VacTrack
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         #endregion
 
-        private ObservableCollection<Employee> _employees;
+        private ObservableCollection<Employee> _employees = null!;
         public ObservableCollection<Employee> Employees
         {
             get => _employees;
             set => SetProperty(ref _employees, value);
         }
 
+        private ObservableCollection<Users> _users = null!;
+        private Users _currentUser = null!;
+        public Users CurrentUser
+        {
+            get => _currentUser;
+            set => SetProperty(ref _currentUser, value);
+        }
+
+        private string? _message;
+        public string? Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
+
         private Employee? _selectedAccountant;
-        public Employee? SelectedAccountant 
+        public Employee? SelectedAccountant
         {
             get => _selectedAccountant;
             set => SetProperty(ref _selectedAccountant, value);
         }
         private Employee? _selecteStorekeeper;
-        public Employee? SelecteStorekeeper 
+        public Employee? SelecteStorekeeper
         {
             get => _selecteStorekeeper;
             set => SetProperty(ref _selecteStorekeeper, value);
@@ -78,8 +92,18 @@ namespace VacTrack
                 SetProperty(ref _selectNds, value);
             }
         }
-        
-        public bool IsDarkTheme {  
+        private string _selectCurrency = Properties.Settings.Default.Currency;
+        public string SelectCurrency
+        {
+            get => _selectCurrency;
+            set
+            {
+                SetProperty(ref _selectCurrency, value);
+            }
+        }
+
+        public bool IsDarkTheme
+        {
             get => SelectTheme == BaseTheme.Dark;
             set
             {
@@ -87,7 +111,8 @@ namespace VacTrack
                 SelectTheme = BaseTheme.Dark;
             }
         }
-        public bool IsLightTheme {
+        public bool IsLightTheme
+        {
             get => SelectTheme == BaseTheme.Light;
             set
             {
@@ -95,7 +120,8 @@ namespace VacTrack
                 SelectTheme = BaseTheme.Light;
             }
         }
-        public bool IsInheritTheme {
+        public bool IsInheritTheme
+        {
             get => SelectTheme == BaseTheme.Inherit;
             set
             {
@@ -107,7 +133,11 @@ namespace VacTrack
         private readonly PaletteHelper _paletteHelper = new();
         private readonly Theme _theme;
         private readonly DatabaseContext Db = new();
-        
+
+        public string? OldPasswordString { get; set; }
+        public string? NewPasswordString { get; set; }
+        public string? NewSecondPasswordString { get; set; }
+
         private BaseTheme _thisTheme;
         private BaseTheme SelectTheme
         {
@@ -117,7 +147,7 @@ namespace VacTrack
                 SetProperty(ref _thisTheme, value);
                 _theme.SetBaseTheme(_thisTheme);
                 _paletteHelper.SetTheme(_theme);
-                
+
                 OnPropertyChanged(nameof(IsDarkTheme));
                 OnPropertyChanged(nameof(IsLightTheme));
                 OnPropertyChanged(nameof(IsInheritTheme));
@@ -126,6 +156,7 @@ namespace VacTrack
 
         public ICommand CancelCommand { get; set; }
         public ICommand SaveCommand { get; set; }
+        public ICommand ChangePassword {  get; set; }
 
         public SettingViewModel()
         {
@@ -133,11 +164,21 @@ namespace VacTrack
 
             CancelCommand = new RelayCommand(Cancel);
             SaveCommand = new RelayCommand(Save);
+            ChangePassword = new RelayCommand(ChangePass);
 
             Init();
-            
-            if (Employees == null || _employees == null) 
-                throw new ArgumentNullException(nameof(Employees));
+
+            if (_currentUser == null ||
+                _users == null ||
+                _employees == null)
+            {
+                Properties.Settings.Default.LogInUserId = -1;
+                Properties.Settings.Default.Save();
+#if !DEBUG
+                MessageBox.Show("Не удалось загрузить пользователя. Возможно, сохранённый ID недействителен.\n" +
+                    "Сохранённый пользователь не найден. Выполните вход заново.");
+#endif
+            }
         }
 
         private void Init()
@@ -145,17 +186,21 @@ namespace VacTrack
             LoadData();
 
             SelectTheme = Properties.Settings.Default.AppTheme switch
-                            {
-                                "Dark" => BaseTheme.Dark,
-                                "Light" => BaseTheme.Light,
-                                "Inherit" => BaseTheme.Inherit,
-                                _ => BaseTheme.Inherit,
-                            };
+            {
+                "Dark" => BaseTheme.Dark,
+                "Light" => BaseTheme.Light,
+                "Inherit" => BaseTheme.Inherit,
+                _ => BaseTheme.Inherit,
+            };
             SelectedAccountant = Employees.FirstOrDefault(e => e.Id == Properties.Settings.Default.ResponsibleAccountant);
             SelecteStorekeeper = Employees.FirstOrDefault(e => e.Id == Properties.Settings.Default.ResponsibleStorekeeper);
             SelectNds = Properties.Settings.Default.Nds;
             SelectedProductReleaseApprover = Employees.FirstOrDefault(e => e.Id == Properties.Settings.Default.ProductReleaseApprover);
             SelectedProductSubmitter = Employees.FirstOrDefault(e => e.Id == Properties.Settings.Default.ProductSubmitter);
+            SelectCurrency = Properties.Settings.Default.Currency;
+
+            var tempUsr = _users.FirstOrDefault(u => u.Id == Properties.Settings.Default.LogInUserId);
+            if (tempUsr != null) CurrentUser = tempUsr;
         }
 
         private void Cancel(object obj) => Init();
@@ -165,6 +210,10 @@ namespace VacTrack
             var emplDbSet = Db.Set<Employee>();
             emplDbSet.Include(m => m.Post).Load();
             Employees = emplDbSet.Local.ToObservableCollection();
+
+            var usersDbSet = Db.Set<Users>();
+            usersDbSet.Load();
+            _users = usersDbSet.Local.ToObservableCollection();
         }
 
         public void ReloadData() => LoadData();
@@ -172,26 +221,63 @@ namespace VacTrack
         private void Save(object obj)
         {
             if (
-                SelectedAccountant == null || 
+                SelectedAccountant == null ||
                 SelecteStorekeeper == null ||
                 SelectedProductReleaseApprover == null ||
                 SelectedProductSubmitter == null
                 ) return;
-            
+
             Properties.Settings.Default.ResponsibleAccountant = SelectedAccountant.Id;
             Properties.Settings.Default.ResponsibleStorekeeper = SelecteStorekeeper.Id;
             Properties.Settings.Default.AppTheme = SelectTheme switch
-                            {
-                                BaseTheme.Dark => "Dark",
-                                BaseTheme.Light => "Light",
-                                BaseTheme.Inherit => "Inherit",
-                                _ => "Inherit"
-                            };
+            {
+                BaseTheme.Dark => "Dark",
+                BaseTheme.Light => "Light",
+                BaseTheme.Inherit => "Inherit",
+                _ => "Inherit"
+            };
             Properties.Settings.Default.Nds = SelectNds;
             Properties.Settings.Default.ProductReleaseApprover = SelectedProductReleaseApprover.Id;
             Properties.Settings.Default.ProductSubmitter = SelectedProductSubmitter.Id;
+            Properties.Settings.Default.Currency = SelectCurrency;
 
             Properties.Settings.Default.Save();
+            Db.SaveChanges();
+        }
+
+        private void ChangePass(object obj)
+        {
+            if(OldPasswordString == null || NewPasswordString == null || NewSecondPasswordString == null)
+            {
+                Message = "поля для пароля пусты";
+                return;
+            }
+
+            if (NewPasswordString != NewSecondPasswordString)
+            {
+                Message = "пароли не совпадают";
+                return;
+            }
+
+            string oldPassHash = CreateUserViewModel.HashPassword(OldPasswordString);
+            if (oldPassHash == CurrentUser.Password)
+            {
+                string newPassHash = CreateUserViewModel.HashPassword(NewPasswordString);
+                CurrentUser.Password = newPassHash;
+                Db.SaveChanges();
+                Message = "Пароль изменён";
+
+                OldPasswordString = string.Empty; OnPropertyChanged(nameof(OldPasswordString));
+                NewPasswordString = string.Empty; OnPropertyChanged(nameof(NewPasswordString));
+                NewSecondPasswordString = string.Empty; OnPropertyChanged(nameof(NewSecondPasswordString));
+
+                return;
+            }
+            else
+            {
+                Message = "Неверный старый пароль";
+                return;
+            }
         }
     }
 }
